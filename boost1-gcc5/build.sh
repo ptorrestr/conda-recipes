21a4e148b6df10909f2d41241f5224789aebc7e6
Pablo Torres <pablo.torres@insight-centre.org>
2017-01-20 13:23:55 +0000

    update std for boost

--- boost1-gcc5/build.sh
+++ boost1-gcc5/build.sh
@@ -14,9 +14,9 @@ INCLUDE_PATH="${PREFIX}/include"
 LIBRARY_PATH="${PREFIX}/lib"
 
 if [ "$(uname)" == "Darwin" ]; then
-    MACOSX_VERSION_MIN=10.6
+    MACOSX_VERSION_MIN=10.9
     CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    CXXFLAGS="${CXXFLAGS} -stdlib=libstdc++"
+    CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
     LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
     LINKFLAGS="${LINKFLAGS} -stdlib=libstdc++ -L${LIBRARY_PATH}"
 
