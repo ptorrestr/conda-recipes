b1043ea2b4dc206cab8f0884c434ece25e68ae18
Pablo Torres <pablo.torres@insight-centre.org>
2017-01-20 14:44:16 +0000

    fix std version

--- hdtconnector/build.sh
+++ hdtconnector/build.sh
@@ -2,12 +2,29 @@
 
 set -x -e
 
-if [ "$(uname)" == "Linux" ]; then
-  ln -s ${PREFIX}/lib ${PREFIX}/lib64
-  ls ${PREFIX}/include
+INCLUDE_PATH="-I${PREFIX}/include"
+LIBRARY_PATH="-L${PREFIX}/lib"
+
+if [ "$(uname)" == "Darwin" ]; then
+  MACOSX_VERSION_MIN=10.9
+  CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -ftemplate-depth-100 -std=c++11"
+  LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -L${LIBRARY_PATH}"
+
   HDT_CFLAGS=${PREFIX}/include \
     HDT_LIBS=${PREFIX}/lib \
     BOOST_ROOT=${PREFIX} \
     PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" \
     python setup.py install
+fi
+
+if [ "$(uname)" == "Linux" ]; then
+  ln -s ${PREFIX}/lib ${PREFIX}/lib64
+  HDT_CFLAGS=${PREFIX}/include \
+    HDT_LIBS=${PREFIX}/lib \
+    BOOST_ROOT=${PREFIX} \
+    PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" \
+    python setup.py install
+  unlink ${PREFIX}/lib64
 fi
