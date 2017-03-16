5c70e402599e134f84e80fe346c80d030bec05d3
Pablo Torres <pablo.torres@insight-centre.org>
2017-03-16 11:12:35 +0000

    add binding for librdf

---
+++ librdf-binding/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x -e
+
+INCLUDE_PATH="-I${PREFIX}/include"
+LIBRARY_PATH="-L${PREFIX}/lib"
+
+if [ "$(uname)" == "Darwin" ]; then
+  MACOSX_VERSION_MIN=10.9
+  CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+  LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -L${LIBRARY_PATH}"
+  CXX=llvm-g++
+  CC=llvm-gcc
+  export LIBTOOLIZE=/opt/local/bin/glibtoolize
+fi
+if [ "$(uname)" == "Linux" ]; then
+  CXX=g++
+  CC=gcc
+fi
+
+ln -s ${PREFIX}/lib ${PREFIX}/lib64
+
+aclocal -I m4 --install
+autoreconf --install --force
+./configure --with-python --prefix=$PREFIX
+make \
+  | tee conda.make.log 2>&1
+make install \
+  | tee conda.make-install.log 2>&1
+unlink ${PREFIX}/lib64
