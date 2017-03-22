3f84fbdfb6e8c9122f74c383517e39b85e4c9e1d
Pablo Torres <pablo.torres@insight-centre.org>
2017-03-22 11:36:28 +0000

    librdf-python

--- librdf-python-binding/build.sh
+++ librdf-python-binding/build.sh
@@ -22,11 +22,8 @@ fi
 
 ln -s ${PREFIX}/lib ${PREFIX}/lib64
 
-#./autogen.sh --with-python=$PYTHON --with-python-ldflags="$(python-config --ldflags)" --prefix=$PREFIX
+PYTHON_LDFLAGS=$(python-config --ldflags) ./autogen.sh --with-python=$PYTHON --prefix=$PREFIX
 
-aclocal -I m4 --install
-autoreconf --install --force
-./configure --with-python=$PYTHON --with-python-ldflags="$(python-config --ldflags)"  --prefix=$PREFIX
 make \
   | tee conda.make.log 2>&1
 make install \
