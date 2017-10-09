f3b47944f0962ee303794b2f4827a4af5a610822
Pablo Torres <pablo.torres@insight-centre.org>
2017-10-09 15:41:28 +0100

    include serd

---
+++ serd/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -x -e
+
+INCLUDE_PATH="-I${PREFIX}/include"
+LIBRARY_PATH="-L${PREFIX}/lib"
+
+if [ "$(uname)" == "Darwin" ]; then
+  MACOSX_VERSION_MIN=10.11
+  CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+  LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -L${LIBRARY_PATH}"
+  CXX=llvm-g++
+	CC=llvm-gcc
+  ln -s ${PREFIX}/lib ${PREFIX}/lib64
+  ./waf configure \
+    --prefix="${PREFIX}" \
+    LDFLAGS=${LIBRARY_PATH} \
+    CPPFLAGS=${INCLUDE_PATH} \
+    PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" \
+    |  tee conda.configure.log 2>&1
+  ./waf \
+    | tee conda.make.log 2>&1
+  ./waf install \
+    | tee conda.make-install.log 2>&1
+  unlink ${PREFIX}/lib64
+fi
+
+if [ "$(uname)" == "Linux" ]; then
+  ln -s ${PREFIX}/lib ${PREFIX}/lib64
+  ./waf configure \
+    --prefix="${PREFIX}" \
+    CC=gcc \
+    CXX=g++ \
+    LDFLAGS=${LIBRARY_PATH} \
+    CPPFLAGS=${INCLUDE_PATH} \
+    PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" \
+    |  tee conda.configure.log 2>&1
+  ./waf -j$(nproc) \
+    | tee conda.make.log 2>&1
+  ./waf install \
+    | tee conda.make-install.log 2>&1
+  unlink ${PREFIX}/lib64
+fi
