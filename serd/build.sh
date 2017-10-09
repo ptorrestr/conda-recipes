96af1abd797717264f21d8a91ef3a1fce43dba2d
Pablo Torres <pablo.torres@insight-centre.org>
2017-10-09 15:53:54 +0100

    use gcc defined by conda

--- serd/build.sh
+++ serd/build.sh
@@ -11,8 +11,6 @@ if [ "$(uname)" == "Darwin" ]; then
   CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
   LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
   LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -L${LIBRARY_PATH}"
-  CXX=llvm-g++
-	CC=llvm-gcc
   ln -s ${PREFIX}/lib ${PREFIX}/lib64
   ./waf configure \
     --prefix="${PREFIX}" \
@@ -31,8 +29,6 @@ if [ "$(uname)" == "Linux" ]; then
   ln -s ${PREFIX}/lib ${PREFIX}/lib64
   ./waf configure \
     --prefix="${PREFIX}" \
-    CC=gcc \
-    CXX=g++ \
     LDFLAGS=${LIBRARY_PATH} \
     CPPFLAGS=${INCLUDE_PATH} \
     PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" \
